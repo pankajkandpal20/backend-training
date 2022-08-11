0f01aa0f6eb17fb6300c2e761cda386924912825
Pankaj <pkandpal601@gmail.com>
2022-08-11 16:47:31 +0530

    upload

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,35 @@
 const express = require('express');
+const _ = require('underscore')
+
 const abc = require('../introduction/intro')
+const loggerModule = require('../logger/logger.js')
+const formatterModule = require('../validator/formatter') 
+const helperModule = require('../util/helper')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
-    logger.welcome()
-
+    loggerModule.printInfo()
+    formatterModule.trimMyString()
+    formatterModule.getUpperCaseString()
+    formatterModule.changetoLowerCase()
+    helperModule.getTodaysDate()
+    helperModule.getCurrentMonth()
+    helperModule.printBatchDetails()
+    let weekdend = ['Saturday','Sunday','Monday']
+    let result = _.first(weekdend, 2)
+    console.log('Unserscore example resultr is ',result)
     res.send('My second ever api!')
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+
+router.get('/test-you', function(req, res){
+    res.send('This is the second routes implementation')
 })
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
+router.get('/give-me-students-data',function(req, res){
 
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
 })
-
 module.exports = router;
+// adding this comment for no reason
