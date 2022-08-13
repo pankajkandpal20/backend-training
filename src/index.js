c78d5c1ddc8a29447803a11fb5569459e12163fd
Pankaj <pkandpal601@gmail.com>
2022-08-13 18:03:49 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
