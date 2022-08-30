9fb0097294c6d34435c6770d38a914a7b01f8d0a
Pankaj <pkandpal601@gmail.com>
2022-08-30 14:56:53 +0530

    solution assignment

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
-
+//Authentication JWT
+//Identify of the users are checked for providing access to the system (Login)
 const validateToken = function(req, res, next) {
     let token = req.headers["x-Auth-token"];
     if (!token) token = req.headers["x-auth-token"];
@@ -18,7 +19,18 @@ const validateToken = function(req, res, next) {
     if (!decodedToken) {
       return res.send({ status: false, msg: "token is invalid" });
     }
+    req.loggedInUser = decodedToken.userId
     next()
+}
+//Authorization 
+//Users authorities are checked for accessing the resource
+const checkAuthorized = function (req, res, next) {
+  let requestedUserId = req.params.requestedUserId
+  if(requestedUserId != req.loggedInUser){
+     return res.send({status: false, msg: "Permission denied"})
+  }
+  next();
 }
 
 module.exports.validateToken = validateToken
+module.exports.checkAuthorized = checkAuthorized
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
-const auth = require("../middlewares/auth")
+const {validateToken, checkAuthorized} = require("../middlewares/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -12,10 +12,10 @@ router.post("/users", userController.createUser  )
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", auth.validateToken, userController.getUserData)
+router.get("/users/:userId", validateToken, checkAuthorized, userController.getUserData)
 
-router.put("/users/:userId", auth.validateToken, userController.updateUser)
+router.put("/users/:userId", validateToken, checkAuthorized, userController.updateUser)
 
-router.delete("/users/:userId", auth.validateToken, userController.deleteUser)
+router.delete("/users/:userId", validateToken, checkAuthorized, userController.deleteUser)
 
 module.exports = router;
